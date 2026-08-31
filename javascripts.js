d0a4041ed73dd5dec6baa33a9694e1a0206dafbb
pakipakdaman <parsa1395pakdaman@gmail.com>
2026-08-31 21:07:50 +0330

    added html file

--- javascripts.js
+++ javascripts.js
@@ -5,3 +5,46 @@ const islearningjs= true;
 console.log(name);
 console.log(age);
 console.log(islearningjs);
+
+
+
+function sum(a,b){return a + b;}      
+const result = sum(5,10);
+console.log(result);
+
+
+function IsEven(number2){return number2 % 2 === 0}
+const result2 = IsEven(10)
+console.log(result2)
+
+
+
+const number3 = 75;
+if (number3 >= 90){
+        console.log("excellent")
+} else if (number3 >= 70){
+        console.log("good")
+} else{
+        console.log("keeplearning")
+}
+
+const fruits = ["apple","orange","blueberry"];
+console.log(fruits[0])
+console.log(fruits)
+fruits.push("banana")
+console.log(fruits)
+console.log(fruits.length)
+for(const fruit of fruits){
+        console.log(fruit);
+}
+for (let i=1; i<=5; i++){
+        console.log(i);
+}
+
+const car = {
+        brand : "toyota",
+        model : "x12",
+        year : 2024
+}
+console.log(car.brand)
+console.log(car)
